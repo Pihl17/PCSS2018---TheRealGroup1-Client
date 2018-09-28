24c602963957be4abe262d60f78dae45aef9b71f
MPihl <mpihl17@student.aau.dk>
2018-09-28 14:52:34 +0200

    New branch and GUI class with DisplayChat function

---
+++ src/Client/GUI.java
@@ -0,0 +1,24 @@
+package Client;
+
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+
+public class GUI {
+	
+	static JPanel panel = new JPanel(new GridLayout(1, 1));
+
+	public static void DisplayChat(String... strings) {
+		panel = new JPanel(new GridLayout(strings.length, 1));
+		for (String string : strings) {
+			JLabel label = new JLabel(string);
+			panel.add(label);
+		}
+		JFrame frame = new JFrame("Chat");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
