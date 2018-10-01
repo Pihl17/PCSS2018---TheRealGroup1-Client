34b11198e6831f39439fe6545afe5d96e725159a
MPihl <mpihl17@student.aau.dk>
2018-10-01 16:31:25 +0200

    Update-able chat

--- src/Client/GUI.java
+++ src/Client/GUI.java
@@ -4,21 +4,30 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.Box;
 
 public class GUI {
 	
-	static JPanel panel = new JPanel(new GridLayout(1, 1));
+	static JPanel chatPanel = new JPanel(new GridLayout(1, 1));
+	static JFrame frame = new JFrame("Chat");
 
-	public static void DisplayChat(String... strings) {
-		panel = new JPanel(new GridLayout(strings.length, 1));
-		for (String string : strings) {
-			JLabel label = new JLabel(string);
-			panel.add(label);
-		}
-		JFrame frame = new JFrame("Chat");
+	public static void DisplayChat() {
+		chatPanel = new JPanel();
+		chatPanel.setLayout(new BoxLayout(chatPanel, BoxLayout.Y_AXIS));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(panel);
-		frame.pack();
+		frame.add(chatPanel);
+		frame.setSize(500, 200);
 		frame.setVisible(true);
+	}
+
+	public static void UpdateChat(String... chatLog) {
+		chatPanel.removeAll();
+		for (String string : chatLog) {
+			JLabel label = new JLabel(string);
+			chatPanel.add(label);
+		}
+		chatPanel.revalidate();
+		chatPanel.updateUI();
 	}
 }
--- src/Client/Main.java
+++ src/Client/Main.java
@@ -3,6 +3,14 @@ package Client;
 public class Main {
 
 	public static void main(String[] args) {
-		GUI.DisplayChat("Test1", "Test2");
+		GUI.DisplayChat();
+		try {
+			String[] chatLog = {"Test1", "Test2", "Test3", "Test4"};
+			GUI.UpdateChat("Test1","Test2");
+			Thread.sleep(5000);
+			GUI.UpdateChat("Test1","Test2","Test3");
+			Thread.sleep(1000);
+			GUI.UpdateChat(chatLog);
+		} catch (InterruptedException e) {}
 	}
 }
