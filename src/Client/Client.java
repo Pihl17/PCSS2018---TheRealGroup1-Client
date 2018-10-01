8d8d5c429ded7d18be6c76094f43e4d36428bcb4
TonyTugger <33148412+TonyTugger@users.noreply.github.com>
2018-10-01 16:59:25 +0200

    Updated player class and began Client/Server communication

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,1 +1,23 @@
 package Client;
+
+import java.io.*;
+import java.net.*;
+import javafx.application.Application;
+import java.util.Date;
+
+Socket socket = new Socket(host, 1916);
+
+int port = 1916;
+String host = "localhost"
+DataInputStream in;
+DataOutputStream out;
+
+
+
+in = new DataInputStream
+	(socket.getInputStream());
+out = new DataOutputStream
+	(socket.getOutputStream());
+out.writeDouble(aNumber);
+System.out.println(in.readDouble());
+
