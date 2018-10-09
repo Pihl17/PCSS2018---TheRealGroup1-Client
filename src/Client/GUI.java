7c4a600ee86d15f0e24cbb9364aa72d1bd5f061b
MPihl <mpihl17@student.aau.dk>
2018-10-09 11:29:16 +0200

    Made it possible to combine the panes into the same scene

--- src/Client/GUI.java
+++ src/Client/GUI.java
@@ -4,27 +4,43 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.scene.control.Label;
 
 public class GUI extends Application implements GUIConstants {
 
-	Scene chatScene = new Scene(new VBox());
+	GridPane mainPane = new GridPane();
+	Scene mainScene = new Scene(mainPane);
 	VBox chatlayout = new VBox();
+
 	
 	public void init() throws Exception {
 		//Runs before the start function
 	}
 
 	public void start(Stage stage) throws Exception {
-		chatScene = new Scene(chatlayout);
-		chatlayout.getChildren().addAll(new Label("label 1"), new Label("label 2"));
+		AddToChat(new Label("label 1"), new Label("label 2"));
 
-		//TODO Include grid window
+		//VBox box2 = new VBox();
+		//box2.getChildren().addAll(new Label("label 1-2"), new Label("label 2-2"));
+
+		//TODO Include write to chat pane
+
+		//TODO Include grid pane
+		
+		//TODO Include Lobby pane
+
+		mainPane = new GridPane();
+		mainScene = new Scene(mainPane);
+		mainPane.setVgap(10);
+		mainPane.setHgap(10);
+		mainPane.add(chatlayout, 0, 0);
+		//mainPane.add(box2, 1, 0);
 
 		stage.setTitle("DND and stuff!");
 		stage.setWidth(WINDOW_WIDTH);
 		stage.setHeight(WINDOW_HEIGHT);
-		stage.setScene(chatScene);
+		stage.setScene(mainScene);
 		stage.show();
 
 		//AddToChat(new Label("label 3"), new Label("label 4"));
