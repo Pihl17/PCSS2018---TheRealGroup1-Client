acb00b6caf14ca74bf5fa012ccff7cd333add634
TonyTugger <33148412+TonyTugger@users.noreply.github.com>
2018-10-09 09:42:19 +0200

    Client updates and Player baseline

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,23 +1,51 @@
 package Client;
 
-import java.io.*;
-import java.net.*;
-import javafx.application.Application;
-import java.util.Date;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
-Socket socket = new Socket(host, 1916);
+public class Client {
+	public static void main(String[] args) {
 
-int port = 1916;
-String host = "localhost"
-DataInputStream in;
-DataOutputStream out;
+   	 // int port = 8000;
+	 // String host = "localhost";
+		DataInputStream in;
+		DataOutputStream out;
+		Socket socket;
+		boolean cont = true;
 
+		Scanner input = new Scanner(System.in);
 
+		try {
+			socket = new Socket("localhost", 1916);
+			System.out.println("Connected to server");
 
-in = new DataInputStream
-	(socket.getInputStream());
-out = new DataOutputStream
-	(socket.getOutputStream());
-out.writeDouble(aNumber);
-System.out.println(in.readDouble());
+			in = new DataInputStream(socket.getInputStream());
 
+			out = new DataOutputStream(socket.getOutputStream());
+
+			while (cont) {
+
+				System.out.println("Welcome");
+
+				
+				
+				System.out.println("Would you like to continue?");
+				System.out.println("1=Yes");
+				System.out.println("2 = no");
+				
+				double exit = input.nextDouble();
+				if (exit == 2) {
+					cont = false;
+					System.out.println("System Terminated");
+					
+				}
+			}
+		}
+
+		catch (IOException e) {
+		}
+	}
+}
