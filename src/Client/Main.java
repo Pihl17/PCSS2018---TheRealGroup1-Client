daff5cf44421b27c44a298fb25692160fadef4e6
Gabaloose <33279909+Gabaloose@users.noreply.github.com>
2018-10-09 12:58:33 +0200

    grid update: emojis and position of mouse

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,13 +1,13 @@
 package Client;
 
+
 public class Main extends Grid {
 
 	public static void main(String[] args) {
 	GUI.DisplayChat("Test1", "Test2");
-		
 	Grid.launch(args); 
-		    
+
+	}
+	
 	}
 
-
-}
